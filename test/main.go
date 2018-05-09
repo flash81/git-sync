20bdba9e145cea30b4ac9e322cf2eb076184638d
feng lee <2684498887@qq.com>
2018-05-09 18:15:33 +0800

    Update main.go
    cmd.Stdin = strings.NewReader("yes\n")

--- test/main.go
+++ test/main.go
@@ -89,8 +89,8 @@ func syncRepo(repo, dest, branch, rev string) error {
 	case os.IsNotExist(err):
 		// clone repo
 		cmd := exec.Command("git", "clone", "--no-checkout", "-b", branch, repo, dest)
-		cmd.Stdin = strings.NewReader("yes\n")
 		output, err := cmd.CombinedOutput()
+		cmd.Stdin = strings.NewReader("yes\n")
 		if err != nil {
 			return fmt.Errorf("error cloning repo %q: %v: %s", strings.Join(cmd.Args, " "), err, string(output))
 		}
